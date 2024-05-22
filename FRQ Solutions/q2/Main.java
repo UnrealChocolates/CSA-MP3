e33e2147f7d58db3713487c9ec597a58fe958ba3
Chocolates <oscarl72@nycstudents.net>
2024-05-22 09:56:56 -0400

    Update Main.java
    Updates Main.java for output purposes

--- FRQ Solutions/q2/Main.java
+++ FRQ Solutions/q2/Main.java
@@ -1,8 +1,32 @@
-
 public class Main {
   public static void main(String[] args) {
-    System.out.println("FRQ question 2");
+    System.out.println("2024 AP CSA FRQ Question 2:");
+
+    // String info;
+    // Scoreboard game = new Scoreboard("Red", "Blue");
+    // info = game.getScore(); 
+    
+    // game.recordPlay(1); 
+    // info = game.getScore(); 
+    
+    // game.recordPlay(0); 
+    // info = game.getScore(); 
+    // info = game.getScore(); 
+    
+    // game.recordPlay(3); 
+    // info = game.getScore(); 
+    
+    // game.recordPlay(1); 
+    // game.recordPlay(0);
+    // info = game.getScore();
+    
+    // game.recordPlay(0); 
+    // game.recordPlay(4); 
+    // game.recordPlay(0); 
+    // info = game.getScore();
+    
+    // Scoreboard match =new Scoreboard("Lions", "Tigers");
+    // info = match.getScore(); "0-0-Lions"
+    // info = game.getScore(); 
   }
-
-
 }
